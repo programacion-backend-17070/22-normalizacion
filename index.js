5007370c3575e0eab0f73877c7c59c5c45cd9165
lalomx <lalo0892@gmail.com>
2022-03-23 07:15:54 -0600

    clase

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
 
 
 (async () => {
-  const { normalize, denormalize, schema } = require("normalizr")
-  const fs = require("fs/promises")
-  const { print } = require("./util")
-  
-  const data = JSON.parse((await fs.readFile("./movies.json")))
 
-  const director = new schema.Entity("directors");
-  const actor = new schema.Entity("actors")
-  const movie = new schema.Entity("movies", {
-    director: director,
-    actors: [actor]
-  })
-
-  const user = new schema.Entity("users", {
-    movies: [movie]
-  })
-
-  const normalizedData = normalize(data, user)
-
-  print(normalizedData)
-  console.log("-------------")
-
-  const denormalizedData = denormalize(1, user, normalizedData.entities)
-
-  print(denormalizedData)
 })()
