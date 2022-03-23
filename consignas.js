158583706fb53d7882b075400ec0e41708210f32
lalomx <lalo0892@gmail.com>
2022-03-23 10:40:31 -0600

    clase en vivo

---
+++ consignas.js
@@ -0,0 +1,32 @@
+// 1-
+
+router.get("/api/productos-test", () => {
+  const productos = []
+  for ( 0 al 4) {
+    productos.push({
+      objeto con faker
+    })
+  }
+
+  res.send(productos)
+})
+
+// 2
+id: 1
+mensajes: [
+  {
+    author: {
+      id: "",
+      nombre: ""
+    },
+    text: ""
+  }
+]
+
+user
+mensaje, {
+  author: user
+}
+
+socket.emit("mensajes", normalizedData)
+
--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-
-
 (async () => {
+  const { normalize, denormalize, schema } = require("normalizr")
+  const fs = require("fs/promises")
+  const { print } = require("./util")
+
+  const data = JSON.parse(await fs.readFile("./movies.json", "utf8"))
+
+  const director = new schema.Entity("directors", { idAttribute: "dni" })
+  const actor = new schema.Entity("actors")
+  const movie = new schema.Entity("movies", {
+    director: director,
+    actors: [actor]
+  })
+  const user = new schema.Entity("users", {
+    movies: [movie]
+  })
+
+  const normalizedData = normalize(data, user)
+
+  print(normalizedData)
+
+  // console.log(JSON.stringify(data).length, JSON.stringify(normalizedData.entities.movies[normalizedData.result]).length)
+
+  const denormalizedData = denormalize(1, user, normalizedData.entities)
+
+  print(denormalizedData)
 
 })()
